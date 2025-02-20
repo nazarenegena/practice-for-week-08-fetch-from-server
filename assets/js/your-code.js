9d8fbaa2d7962cec9b49deee49ad9817d7eb82fb
Nazarene <nazarenegena@users-MacBook-Pro.local>
2025-02-20 12:54:58 +0300

    refactored code

--- assets/js/your-code.js
+++ assets/js/your-code.js
@@ -1,19 +1,82 @@
-export function getAllDogs() {
-    // Your code here
+export async function getAllDogs() {
+  try {
+    const res = await fetch("/api/dogs");
+
+    // Handle HTTP errors
+    if (!res.ok) {
+      throw new Error(`HTTP error! Status: ${res.status}`);
+    }
+    const data = await res.json();
+    return data; 
+  } catch (error) {
+    console.error("ERROR: ", error);
+    return null; 
+  }
 }
 
-export function getDogNumberTwo() {
-    // Your code here
+export async function getDogNumberTwo() {
+   try {
+     const res = await fetch("/api/dogs/2");
+
+    // Handle HTTP errors
+    if(!res.ok) {
+        throw new Error(`HTTP error! Status: ${res.status}`);
+    }
+    const data = await res.json();
+    return data;
+   } catch (error) {
+    console.error("Error", error);
+    return null;
+   }
 }
 
-export function postNewDog() {
-    // Your code here
+export async function postNewDog() {
+   try {
+
+    const res = await fetch ("/api/dogs", {
+        method: "POST",
+        headers: headers,
+        body:body
+    })
+    const data = await res.json();
+    return data;
+   } catch (error) {
+    console.error("Error", error);
+    return null;
+   }
 }
 
-export function postNewDogV2(name, age) {
-     // Your code here
+export async function postNewDogV2(name, age) {
+     try {
+const response = await fetch("/api/dogs", {
+    method: "POST",
+    headers : {
+        "Content-Type": "application/json"
+    
+    },
+    body: JSON.stringify({
+        name: name,
+        age: age
+    })
+})
+const data = await response.json();
+return data;
+     } catch (err) {
+        console.log("Error", err);
+        return null;
+     }
 }
 
-export function deleteDog(id) {
-      // Your code here
+export async function deleteDog(id) {
+      try {
+        const response = await fetch(`/api/dogs/${id}`, {
+            method: "DELETE",
+           
+        })
+const data = await response.json();
+return data;
+      } catch (err) {
+        console.error("Error", err);
+        return null
+      }
 }
